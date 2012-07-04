04985d360ce978dd38badf3c24328423f60d5cd9
David <davidcp90@gmail.com>
2012-07-04 17:38:47 -0500

    SE añadieron los methods para mg1

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,8 @@
 /**
  * @author David Felipe Camargo Polo
  */
+//FUNCIONES PARA MODELOS
+//Para M/G/1
 function tiempoMedio(lambda){
 	var tiempoMed=1/lambda;
 	return tiempoMed;
@@ -28,4 +30,19 @@ function tiempoCola(lambda,mu){
 function probVacio(lambda,mu){
 	var probV=1-(lambda-mu);
 	return probV;
+}
+//Para M/G/1
+function ro(lambda,mu){
+	var ro=lambda/mu;
+	return ro;
+}
+function colaMediaPK(lambda,mu,omi){
+	var roc=ro(lambda,mu);
+	var colaMPK=roc+(((roc*roc)+(lambda*lambda)*(omi))/2*(1-roc));
+	return colaMPK;
+}
+function tiempocolaPK(lambda,mu,omi){
+	var roc=ro(lambda,mu);
+	var tiPK=((roc*roc)+(lambda*lambda)*(omi))/(2*lambda)*(1-roc);
+	return tiPK;
 }
--- js/interaccion.js
+++ js/interaccion.js
@@ -4,6 +4,7 @@
 $('#calc1').click(function(){
 	var lambda1 = $("input#lambda1").val();
 	var mu1 = $("input#mu1").val();
+	if(lambda2!=0 && mu2!=0 && omi2!=0){
 	if(lambda1<mu1){
 		
 	var rtiempoMedio=tiempoMedio(lambda1);
@@ -34,11 +35,94 @@ $('#calc1').click(function(){
 		$('#error1').html("Revisa el problema &lambda; debe ser menor que &mu; para operar");
 		$('#error1').fadeIn('slow');
 	}
+}
+	else{
+		$('#error1').html("Los valores no pueden ser cero");
+		$('#error1').fadeIn('slow');
+	}
 	
 });
 $('#calc2').click(function(){
+	var lambda2 = $("input#lambda2").val();
+	var mu2 = $("input#mu2").val();
+	var omi2 = $("input#omi2").val();
+	if(lambda2!=0 && mu2!=0 && omi2!=0){
+	if(lambda2<mu2){
+		var rtiempoMedio=tiempoMedio(lambda2);
+	$("#tmedr2").text(rtiempoMedio);
 	
+	var rro=ro(lambda2,mu2);
+	$("#ror2").text(rro);
+	
+	var rtiempoMedioServ=tiempoMedioServ(mu2);
+	$("#tmedservr2").text(rtiempoMedioServ);
+	
+	var rclienteSist=clienteSist(lambda2,mu2);
+	$("#cantespr2").text(rclienteSist);
+	
+	var colaPK=colaMediaPK(lambda2,mu2,omi2);
+	$("#cantespcolr2").text(colaPK);
+	
+	var rtiempoSist=tiempoSist(lambda2,mu2);
+	$("#tiespr2").text(rtiempoSist);
+	
+	var tiempoPK=tiempocolaPK(lambda2,mu2,omi2);
+	$("#tiespcolr2").text(tiempoPK);
+	
+	var rprobVac=probVacio(lambda2,mu2);
+	$("#probvacr2").text(rprobVac);
+	
+	$('#respuestas2').slideDown('slow');
+	}
+	else{
+		$('#error2').html("Revisa el problema &lambda; debe ser menor que &mu; para operar");
+		$('#error2').fadeIn('slow');
+	}
+	}
+	else{
+		$('#error2').html("Los valores no pueden ser cero");
+		$('#error2').fadeIn('slow');
+	}
 });
 $('#calc3').click(function(){
+	var lambda3 = $("input#lambda3").val();
+	var mu3 = $("input#mu3").val();
+	var omi3 = $("input#omi3").val();
+	if(lambda3!=0 && mu3!=0 && omi3!=0){
+	if(lambda3<mu3){
+		var rtiempoMedio=tiempoMedio(lambda3);
+	$("#tmedr2").text(rtiempoMedio);
 	
+	var rro=ro(lambda2,mu2);
+	$("#ror2").text(rro);
+	
+	var rtiempoMedioServ=tiempoMedioServ(mu2);
+	$("#tmedservr2").text(rtiempoMedioServ);
+	
+	var rclienteSist=clienteSist(lambda2,mu2);
+	$("#cantespr2").text(rclienteSist);
+	
+	var colaPK=colaMediaPK(lambda2,mu2,omi2);
+	$("#cantespcolr2").text(colaPK);
+	
+	var rtiempoSist=tiempoSist(lambda2,mu2);
+	$("#tiespr2").text(rtiempoSist);
+	
+	var tiempoPK=tiempocolaPK(lambda2,mu2,omi2);
+	$("#tiespcolr2").text(tiempoPK);
+	
+	var rprobVac=probVacio(lambda2,mu2);
+	$("#probvacr2").text(rprobVac);
+	
+	$('#respuestas2').slideDown('slow');
+	}
+	else{
+		$('#error2').html("Revisa el problema &lambda; debe ser menor que &mu; para operar");
+		$('#error2').fadeIn('slow');
+	}
+	}
+	else{
+		$('#error2').html("Los valores no pueden ser cero");
+		$('#error2').fadeIn('slow');
+	}
 });
